1028f607a8b6cbaa8ec2d54087ea5d21ceb040cf
StinkySteak <slimesteve17@gmail.com>
2024-11-02 13:35:29 +0700

    feat: narrow phase filtering and refactor visual disabling

--- Assets/_IM/Scripts/Gameplay/Props.cs
+++ Assets/_IM/Scripts/Gameplay/Props.cs
@@ -1,20 +1,32 @@
 using Netick.Unity;
 using UnityEngine;
+using NetworkPlayer = Netick.NetworkPlayer;
 
 namespace StinkySteak.IM.Gameplay
 {
     public class Props : NetworkBehaviour
     {
-        [SerializeField] private MeshRenderer _renderer;
+        [SerializeField] private GameObject _renderer;
+        [SerializeField] private bool _setInterestNarrow;
+
+        public override void NetworkFixedUpdate()
+        {
+            if (!IsServer) return;
+
+            foreach (NetworkPlayer client in Sandbox.ConnectedClients)
+            {
+                Object.SetNarrowphaseInterest(client, _setInterestNarrow);
+            }
+        }
 
         public override void OnBecameInterested()
         {
-            _renderer.SetEnabled(Sandbox, true);
+            _renderer.SetActive(true);
         }
 
         public override void OnBecameUninterested()
         {
-            _renderer.SetEnabled(Sandbox, false);
+            _renderer.SetActive(false);
         }
     }
 }
